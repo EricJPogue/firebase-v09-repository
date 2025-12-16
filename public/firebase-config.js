59668c1e587d94b5978d05ed5312d9280d6a61d5
Eric Pogue <epogue@epogue.com>
2025-12-16 14:02:40 -0600

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -5,12 +5,12 @@ import { getFirestore, doc, setDoc, collection, getDocs } from "https://www.gsta
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCUm9TpdzAyzbzIUknnBZSx2asxzDuAgBA",
+  authDomain: "fir-v09-project.firebaseapp.com",
+  projectId: "fir-v09-project",
+  storageBucket: "fir-v09-project.firebasestorage.app",
+  messagingSenderId: "8651307560",
+  appId: "1:8651307560:web:3a1e13cc4790bb77f8531d"
 };
 
 // Initialize Firebase
